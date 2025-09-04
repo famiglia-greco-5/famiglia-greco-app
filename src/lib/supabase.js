a2ef1b05f3b0b82ac8588115f6758cd577d04540
Famiglia Greco <famigliagreco5@gmail.com>
2025-09-04 19:48:52 +0200

    Fix permessi database Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ylrwdqpgeheiznzbhgjm.supabase.co'
+const supabaseUrl = 'https://ylrwdgpgeheiznzbhgim.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlscndkZ3BnZWhlaXpuemJoZ2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMDI4NzIsImV4cCI6MjA3MjU3ODg3Mn0.SJcpHaNPBGgvlnxNC9WHZrgtMrkALeqP3IlIxX4dAUE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey) 
